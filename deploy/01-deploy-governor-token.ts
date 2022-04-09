ea656a225bd2d1cfbf4a6d06de3ce1687b07c52d
Woojin <cmshim22@gmail.com>
2022-04-10 00:08:07 +0900

    deploy token.ts

---
+++ deploy/01-deploy-governor-token.ts
@@ -0,0 +1,8 @@
+import {HardhatRuntimeEnvironment} from "hardhat/types";
+import {DeployFunction} from "hardhat-deploy/types";
+
+const deployGovernanceToken: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+    console.log("Hello");
+};
+
+export default deployGovernanceToken;

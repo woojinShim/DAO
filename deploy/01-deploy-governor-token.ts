c780b3549456cc747e39f646a0d25238e03c04e2
Woojin <cmshim22@gmail.com>
2022-04-11 21:06:14 +0900

    deploy.ts

--- deploy/01-deploy-governor-token.ts
+++ deploy/01-deploy-governor-token.ts
@@ -3,6 +3,15 @@ import {DeployFunction} from "hardhat-deploy/types";
 
 const deployGovernanceToken: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
     const { getNamedAccounts, deployments, network} = hre;
+    const { deploy, log } = deployments;
+    const {deployer} = await getNamedAccounts();
+    log("Deploying Governance Token...");
+    const governanceToken = await deploy("GovernanceToken", {
+        from: deployer,
+        args: [],
+        log: true,
+    });
+    log(`Deployed governance token to address ${governanceToken.address}`)
 };
 
 export default deployGovernanceToken;

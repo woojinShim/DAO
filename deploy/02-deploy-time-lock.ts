b5f47af6d3717c25d8eec66f09cec019aa50ab81
Woojin <cmshim22@gmail.com>
2022-04-14 01:31:27 +0900

    time-lock2

--- deploy/02-deploy-time-lock.ts
+++ deploy/02-deploy-time-lock.ts
@@ -1,4 +1,6 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
 
-const deployTimeLock: DeployFunction = async function (hre: )
+const deployTimeLock: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+    
+}

b8402efd9c326ae5962e828ff4a292a87d46e162
Woojin <cmshim22@gmail.com>
2022-04-15 15:47:14 +0900

    deploy done

---
+++ deploy/05-deploy-box.ts
@@ -0,0 +1,24 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { ethers } from 'hardhat';
+
+const deployBox: DeployFunction = async function (
+    hre: HardhatRuntimeEnvironment
+) {
+    const { getNamedAccounts, deployments, network } = hre
+    const { deploy, log, get } = deployments
+    const { deployer } = await getNamedAccounts();
+    log("Deploying Box...");
+    const box = await deploy("Box", {
+        from: deployer,
+        args: [],
+        log: true,
+    });
+    const boxContract = await ethers.getContractAt("Box", box.address)
+  const timeLock = await ethers.getContract("TimeLock")
+  const transferTx = await boxContract.transferOwnership(timeLock.address)
+  await transferTx.wait(1)
+    log(" YOU DUN IT!!");
+}
+
+export default deployBox;
